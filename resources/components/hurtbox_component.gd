a47331cf1af9f8593ac53c6d11918882f39ad4da
swksoft <esparzapatricio95@gmail.com>
2024-05-14 09:51:51 -0300

    hurtbox component in progress

---
+++ resources/components/hurtbox_component.gd
@@ -0,0 +1,13 @@
+extends Area2D
+class_name HurtboxComponent
+
+@export var stats_component: StatsComponent
+@export var health_component: HealthComponent
+@export var parent : CharacterBody2D
+
+func _on_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_area_exited(area):
+	pass # Replace with function body.
