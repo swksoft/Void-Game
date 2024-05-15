67e5f00d4e31c6aba818261a64e136fe0664184f
swksoft <esparzapatricio95@gmail.com>
2024-05-15 15:50:10 -0300

    Player tweaks

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,6 +3,11 @@ class_name Player extends CharacterBody2D
 @export var stats_component : StatsComponent
 @export var accel : float = 25.0
 
+# TODO: Turn velocity
+
+func _ready():
+	GameEvents.emit_player_ready()
+
 func _physics_process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
 	
@@ -10,11 +15,11 @@ func _physics_process(delta):
 	velocity.y = move_toward(velocity.y, stats_component.spd * direction.y, accel)
 	
 	if Input.is_action_pressed("left"):
-		rotation -= 5 * delta
+		rotation -= 3 * delta
 	elif Input.is_action_pressed("right"):
-		rotation += 5 * delta
+		rotation += 3 * delta
 		
-	# TODO: JUMP (MAKE SPRITE LARGER AND DISABLE COLLISION) (REPRODUCIR ANIMACION)ehh
+	# TODO: JUMP (MAKE SPRITE LARGER AND DISABLE COLLISION) (REPRODUCIR ANIMACION)
 	if Input.is_action_just_pressed("jump"):
 		pass
 		
