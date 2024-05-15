fd9e0fc19bac7df1e30fc0c045c764f1067db3ba
swksoft <esparzapatricio95@gmail.com>
2024-05-15 15:49:44 -0300

    EVENTS (on work)

--- autoloads/GameEvents.gd
+++ autoloads/GameEvents.gd
@@ -11,23 +11,28 @@ signal stat_update
 signal enemy_hit(enemy: BasicEnemy)
 signal player_ready(player: Player)
 
-
 func emit_wave_start():
+	print("emit_wave_start")
 	wave_start.emit(wave)
 	
 func emit_wave_end():
+	print("emit_wave_end")
 	wave_end.emit(wave)
 
 func emit_enemy_died(wave_spawned: bool):
+	print("emit_wave_died")
 	enemy_died.emit(wave_spawned)
 
 func emit_wave_enemy_limit_reached():
+	print("emit_wave_enemy_limit_reached")
 	wave_enemy_limit_reached.emit()
 
 func emit_stat_update():
+	print("emit_stat_update")
 	stat_update.emit()
 
 func emit_enemy_hit(enemy: BasicEnemy):
+	print("emit_enemy_hit")
 	enemy_hit.emit(enemy)
 
 func emit_player_ready():
