222551f923e59a0ade797e1d8b8d8ee60918cc2a
swksoft <esparzapatricio95@gmail.com>
2024-05-15 15:50:55 -0300

    hud shows stats now

--- ui/hud.gd
+++ ui/hud.gd
@@ -5,20 +5,17 @@ extends CanvasLayer
 var hp : int
 var atk : int
 
-@onready var hp_stat = $Panel/Panel/GridContainer/HPstat
-@onready var atk_stat = $Panel/Panel/GridContainer/ATKstat
-@onready var rng_stat = $Panel/Panel/GridContainer/RNGstat
-@onready var dly_stat = $Panel/Panel/GridContainer/DLYstat
-@onready var dps_stat = $Panel/Panel/GridContainer/DPSstat
-@onready var abr_stat = $Panel/Panel/GridContainer/ABRstat
-@onready var qty_stat = $Panel/Panel/GridContainer/QTYstat
-@onready var spd_stat = $Panel/Panel/GridContainer/SPDstat
+@onready var hp_stat = $Panel/Panel/StatContainer/HPstat
+@onready var atk_stat = $Panel/Panel/StatContainer/ATKstat
+@onready var rng_stat = $Panel/Panel/StatContainer/RNGstat
+@onready var dly_stat = $Panel/Panel/StatContainer/DLYstat
+@onready var dps_stat = $Panel/Panel/StatContainer/DPSstat
+@onready var abr_stat = $Panel/Panel/StatContainer/ABRstat
+@onready var qty_stat = $Panel/Panel/StatContainer/QTYstat
+@onready var spd_stat = $Panel/Panel/StatContainer/SPDstat
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
