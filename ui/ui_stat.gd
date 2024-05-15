880f71f2e4bf96ba0f24cf5bd0bae16eb3932d28
swksoft <esparzapatricio95@gmail.com>
2024-05-15 15:52:08 -0300

    stats script (somehow works)

---
+++ ui/ui_stat.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var stat_name : String
+
+var stats_component : StatsComponent
+
+func _ready():
+	GameEvents.player_ready.connect(on_player_ready) # NO FUNCA
+	
+	on_player_ready()
+
+func update():
+	self.text = str(stats_component["max_" + stat_name])
+	
+func on_player_ready():
+	stats_component = get_tree().get_first_node_in_group("Player").get_node("StatsComponent")
+	GameEvents.stat_update.connect(update)
+	update()
