786eace6587146ffc8423d449e82f12b4ffc1c97
swksoft <esparzapatricio95@gmail.com>
2024-05-18 08:37:41 -0300

    qol update in menu

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,4 +1,9 @@
 extends Control
 
+@onready var button = $CenterContainer/VBoxContainer/Button
+
+func _ready():
+	button.grab_focus()
+
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://tests/test_funcional.tscn")
